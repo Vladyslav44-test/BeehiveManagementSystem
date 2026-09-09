32b56420a49c2d614b129f117472f029cd856b32
Vladislav44-test <elertvelert@gmail.com>
2026-09-09 21:06:43 +0300

    Додав таймер в код програмної частини для переведення гри з покрокового режиму в режим реального часу

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BeehiveManagementSystem
 {
@@ -20,13 +21,21 @@ namespace BeehiveManagementSystem
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Quenn quenn;
+        private Quenn quenn = new Quenn();
+        private DispatcherTimer timer = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
-            quenn = new Quenn();
             statusReport.Text = quenn.StatusReport;
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1.5);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            WorkShift_Click(this, new RoutedEventArgs());
         }
 
         private void AssignJob_Click(object sender, RoutedEventArgs e)
